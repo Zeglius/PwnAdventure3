bf677aa71838f5aaf1c7b8c0d0d1f2708233a5ab
Zeglius <33781398+Zeglius@users.noreply.github.com>
2023-05-06 23:59:48 +0200

    ci: setup beaujeant docker setup
    added zeglius/PwnAdventure3Servers-docker as submodule

--- setup/gameserver.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-su pwn3 -c "cd /opt/pwn3/client/PwnAdventure3_Data/PwnAdventure3/PwnAdventure3/Binaries/Linux/ && ./PwnAdventure3Server"
--- setup/postgres_master.sql
+++
@@ -1,2 +0,0 @@
-UPDATE info SET contents='LiveOverflow' WHERE name='login_title';
-UPDATE info SET contents='Welcome to Pwnie Island!' WHERE name='login_text';
